6652a4819eb96731e9584b6955e19ec37a2e9d8d
Robert McKenzie <rmckenzi@rpmdp.com>
2024-06-05 14:14:17 +0100

    Updated script template

--- scripts/update-vrchours.sh
+++ scripts/update-vrchours.sh
@@ -5,11 +5,11 @@ SRC=<PATH TO YOUR PROFILE REPO ON DISK>
 cd $SRC
 
 # Pull the profile down
-git pull
+gh repo sync
 
 # Fetch the current hours and update the README.md
 # VRChat Game ID: 438100
-./scripts/update-myhours.py <YOUR_STEAM_ID_NUMBER> 438100
+./scripts/update-myhours.py <YOUR_STEAM_ID_NUMBER> <STEAM_GAME_ID>
 
 git add README.md
 git commit -a -m "Update VRC Hours"
